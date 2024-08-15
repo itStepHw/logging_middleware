b42059a7f6c561d68bac13d2f6a7aad3518ace1a
r.bikbaev <r.bikbaev@foranx.com>
2024-08-15 11:15:47 +0500

    minor changes

--- logging_request/app/logger/config.py
+++ logging_request/app/logger/config.py
@@ -3,5 +3,3 @@ import sys
 
 DELIMITER = '\\' if os.name == 'nt' else '/'
 CURRENT_PATH = os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(__file__))
-MODULE_PATH = '../../../../../Desktop/modules'
-CSV_FILES = '_csv_files'
